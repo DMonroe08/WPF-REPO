8dfa38368bccdfe912532c33f280d3fa7b19b0ee
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-19 18:46:04 -0500

    test
    testing a practice commit

---
+++ js.js
@@ -0,0 +1,22 @@
+/**
+ * Created by DanielleMonroe on 2/19/14.
+ */
+
+
+var kidHeight = 52;
+var minHeight = 48;
+var parentHeight = 45
+//if the child is old enough print to the console "you can ride!"
+//if the kid is over 48 in height
+if (kidHeight > minHeight){
+    //you can ride?
+    console.log("You can ride the coaster");
+}else if(kidHeight > parentHeight){
+    //you can ride wih a parent present
+    console.log("You can ride, but only with a parent.");
+}else{
+    //sorry you have growing to do
+    console.log("Sorry kid, you've got some growing to do");
+}
+
+Alert("yay");
