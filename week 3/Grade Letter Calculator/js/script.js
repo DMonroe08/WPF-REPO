1fa6bc2af42056b4a7d7ef05ab6032ab908dda9e
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-20 15:52:14 -0500

    creates variables for all for weeks grades

---
+++ week 3/Grade Letter Calculator/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by DanielleMonroe on 2/20/14.
+ */
+
+//grade Letter Calculator
+
+var week1 = 75; //grade for first week's assignment
+var week2 = 86; //grade for second week's assignment
+var week3 = 90; //grade for third week's assignment
+var week4 = 65; //grade for fourth week's assignment
+
