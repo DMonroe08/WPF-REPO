d45eb472b70a9efd3c7ac1f5e7cbaaf847d35d43
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-20 09:57:21 -0500

    Practice
    Adds “you can ride the coaster!”
    
    Actually works when printed to the console! yay!!

--- week 3/Practice Conditions/js/script.js
+++ week 3/Practice Conditions/js/script.js
@@ -4,10 +4,9 @@
 
 
 
-sunny = true
-
-if(sunny){
-    goToTheBeach();
-}else{
-    goToTheMovies();
+var oldEnough = true;
+//if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("you can ride the coaster!");
 }
