48fff3655d150d0bd1223ebf86cd4224bb7d51be
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-20 14:41:40 -0500

    last chance for gas
    makes variables for given situation

--- week 3/Conditional_Worksheet/js/script.js
+++ week 3/Conditional_Worksheet/js/script.js
@@ -1,17 +1,8 @@
-/**
- * Created by DanielleMonroe on 2/20/14.
- */
+//last Chance for Gas
 
-//must be 250 lbs or more to enter contest
+var mpg = 25; //miles per gallon the car will burn off gas
+var reading = 50; //the amount, in percent, of gas left in the car
+var cap = 12; //the capacity of the gas tank
+var miles = 200; //the amount of miles to the next gas station
+var tank = reading/100 * cap; //the amount of gas left in the tank
 
-var heavyEnough = 285; //the weight of a contestant
-var lightWeight = 249; //the weight of another contestant
-var justRight = 250; //the minimum weight allowed to enter contest
-
-if(heavyEnough > justRight){
-    //allowed to enter the contest
-    console.log("Good luck!");
-}else{
-    //too light
-    console.log("Chunk up and try again next year");
-}
