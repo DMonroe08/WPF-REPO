c67735dcfcd1b5b03c51183bcee729e89a36cfe3
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-20 16:41:50 -0500

    Sets variables and if statements for personal

---
+++ week 3/Conditional_Personal/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by DanielleMonroe on 2/20/14.
+ */
+
+
+//will I go shopping
+
+var bank = 2000; //amount of money in my bank account
+var bills = 1500; //the amount of money my bills will cost
+
+if((bank - bills) > 300){
+    //time for a shopping spree
+    console.log("Time for a shopping spree!");
+}
