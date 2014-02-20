b177e2c6fb55cc71715b06bb22cfa3d67567f698
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-20 16:51:20 -0500

    adds else if factor into the situation

--- week 3/Conditional_Personal/js/script.js
+++ week 3/Conditional_Personal/js/script.js
@@ -8,9 +8,13 @@
 var bank = 2000; //amount of money in my bank account
 var bills = 1500; //the amount of money my bills will cost
 
-if((bank - bills) > 300){
+if((bank-bills) <= 300);{
+    //savings
+    console.log("Save some money and go shopping next month.")
+}elseif((bank - bills) > 300);{
     //time for a shopping spree
     console.log("Time for a shopping spree!");
+
 }else{
     //not enough cash for to buy new clothes
     console.log("Not enough funds for a shopping spree. Mayne next month.")
