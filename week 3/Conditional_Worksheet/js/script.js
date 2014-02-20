fa6e2d6cd2727abbeea4c6c1aee2ce2a543706dc
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-20 10:16:43 -0500

    Made variables and if statements

---
+++ week 3/Conditional_Worksheet/js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by DanielleMonroe on 2/20/14.
+ */
+
+
+var heavyEnough = 250;
+var lightWeight = 249;
+if(heavyEnough){
+    console.log("Welcome to the contest! Good luck!");
+}else
+if(lightWeight){
+    console.log("Chunk up and try again next year!");
+}
