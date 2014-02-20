ddf9b46d4129e7c0e1f362effba94fa1c72d6843
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-20 13:43:10 -0500

    Adds lightweight variable and gives a different outcome

--- week 3/Conditional_Worksheet/js/script.js
+++ week 3/Conditional_Worksheet/js/script.js
@@ -2,12 +2,14 @@
  * Created by DanielleMonroe on 2/20/14.
  */
 
-
+//
 var heavyEnough = 250;
 var lightWeight = 249;
-if(heavyEnough){
+if(heavyEnough > lightWeight){
+    //code performed if the contestant is heavy enough to participate in the contest
     console.log("Welcome to the contest! Good luck!");
 }else
-if(lightWeight){
+if(lightWeight < heavyEnough){
+    //code performed if the contestant is not heavy enough to participate in the contest
     console.log("Chunk up and try again next year!");
 }
