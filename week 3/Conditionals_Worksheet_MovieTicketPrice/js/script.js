e8fe50775f9e2e1dad91ebe5464336c0c6232c74
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-20 20:56:49 -0500

    gives variables

---
+++ week 3/Conditionals_Worksheet_MovieTicketPrice/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by DanielleMonroe on 2/20/14.
+ */
+
+
+var age = prompt("How old are you?"); //gives the age of the customer
+var discount = age <= 10 || age >= 55; //if the age of the customer fall in these perimeters, than the discounted price is given
+var time = prompt ("When will you be joining us for a show?");
+var matinee = time >= 3 || time <= 5; //the discounted price is given
+
