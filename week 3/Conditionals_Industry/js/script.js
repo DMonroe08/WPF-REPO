9447a06986a28b27d59ca4850ad2b4e8fcf74f2f
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-20 19:15:40 -0500

    adds variables for camp

--- week 3/Conditionals_Industry/js/script.js
+++ week 3/Conditionals_Industry/js/script.js
@@ -1,3 +1,13 @@
 /**
  * Created by DanielleMonroe on 2/20/14.
  */
+
+//making a page for a camp
+
+
+var oldEnough = 8; //the minimum age to be able to go to the camp
+var tooYoung = 7; //age that is too young to go to the camp
+var maxAge = 15; //the maximum age allowed to be able to go to the camp
+
+//if the child's age is between 8 and 15 they can go to the camp
+
