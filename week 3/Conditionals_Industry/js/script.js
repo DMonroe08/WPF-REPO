28b6125ae5e68b374a7caf5c3690ab4384bd5e1a
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-20 19:41:33 -0500

    adds child age prompt

--- week 3/Conditionals_Industry/js/script.js
+++ week 3/Conditionals_Industry/js/script.js
@@ -9,12 +9,16 @@ var oldEnough = 8; //the minimum age to be able to go to the camp
 var tooYoung = 7; //age that is too young to go to the camp
 var maxAge = 15; //the maximum age allowed to be able to go to the camp
 
+//how old is your child
+var childAge = prompt("How old is your child?");
+console.log(childAge);
+
 //if the child's age is between 8 and 15 they can go to the camp
 
 if(tooYoung){
     //child can not go to the camp
     console.log("Sorry but your child is not old enough to go the this camp.");
-}else if(oldEnough){
+}else if(oldEnough || maxAge){
     //child can go to the camp
     console.log("Your child is old enough to attend this camp!");
 }
