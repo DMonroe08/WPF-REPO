898c61370289a916307cbcea73bb1d11d8d59216
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-20 21:14:00 -0500

    gives if and else statements

--- week 3/Conditionals_Worksheet_MovieTicketPrice/js/script.js
+++ week 3/Conditionals_Worksheet_MovieTicketPrice/js/script.js
@@ -4,7 +4,15 @@
 
 
 var age = prompt("How old are you?"); //gives the age of the customer
-var discount = age <= 10 || age >= 55; //if the age of the customer fall in these perimeters, than the discounted price is given
+var discountPrice = age <= 10 || age >= 55; //if the age of the customer fall in these perimeters, than the discounted price is given
+var regularFee = age > 10 || age < 55; //if the age of the customer falls in these perimeters than than the regular price is given
+
 var time = prompt ("When will you be joining us for a show?");
 var matinee = time >= 3 || time <= 5; //the discounted price is given
 
+if(age == discountPrice || matinee){
+    //the discounted price is given
+    console.log("The ticket price is $7.");
+}else {
+    console.log("The ticket price is $12 ");
+}
