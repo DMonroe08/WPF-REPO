31a746786e5992a3c992c6973491220b1d3e5c4e
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-20 20:08:28 -0500

    adds variables to wacky condition

--- week 3/Conditionals_Wacky/js/script.js
+++ week 3/Conditionals_Wacky/js/script.js
@@ -1,3 +1,10 @@
 /**
  * Created by DanielleMonroe on 2/20/14.
  */
+
+//Does Gabriel get an extra snack?
+
+var chores = 5; //the amount of chores Gabriel has to do each day
+var done;
+var snack;
+
