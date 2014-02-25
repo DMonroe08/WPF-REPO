034d4ec543aafedcad071bb99787697bc937ca91
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-25 16:22:23 -0500

    Practice Work

---
+++ week 3/Practicing Functions/js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by DanielleMonroe on 2/24/14.
+ */
+
+//function - Anonymous Functions
+
+var calcArea = function(width, height){
+    //code the function runs
+    var area = width * height;
+    return area;
+}
+
+var a = calcArea(20, 30);
+
+console.log(a);
