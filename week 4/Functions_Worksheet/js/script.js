2330de2b1b17ba0525a634e63e39cae0d1f33dd0
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-25 17:45:56 -0500

    sets up functions and gives variables

--- week 4/Functions_Worksheet/js/script.js
+++ week 4/Functions_Worksheet/js/script.js
@@ -1,3 +1,13 @@
 /**
  * Created by DanielleMonroe on 2/25/14.
  */
+
+
+
+function circum(){
+    //circumference is radius times pie
+    var radius = 14;
+    var pie = 3.14;
+    var circum = radius * pie;
+    console.log(circum);
+}
