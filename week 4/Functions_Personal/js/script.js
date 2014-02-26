a4f12ffca15408106335c23c5ee43f24f482eb2c
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-26 16:44:51 -0500

    attempting to fix adding the two prompts
    unsuccessful, going to start over

--- week 4/Functions_Personal/js/script.js
+++ week 4/Functions_Personal/js/script.js
@@ -3,14 +3,3 @@
  */
 //Functions Assignment - Personal
 
-var trip = prompt("How many miles to you travel to work?"); //tells how far the trip to or from work is
-
-var store = prompt("How many miles from work is the store?"); //tells how far the store is from work
-
-function errand(){
-    //how far will it be to go to work, the store, and back home
-    var round = trip + store
-    console.log(round);
-}
-
-errand();
