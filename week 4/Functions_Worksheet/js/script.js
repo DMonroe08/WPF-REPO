632397fedb267260c4974924f863869e030813e4
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-26 13:22:31 -0500

    Replaces prompt window in circumference section as per directions in last weeks comments in graded assignment

--- week 4/Functions_Worksheet/js/script.js
+++ week 4/Functions_Worksheet/js/script.js
@@ -4,10 +4,11 @@
 
 //Circumference
 
-alert("Hello World");
+var r = prompt("What is the radius of your circle?");
 
-function circumference(radius){
+function circumference(){
     //circumference is radius times pie
+    var radius = r;
     var pie = 3.14; //this is how much pie equals
     var circumference = radius * pie; //this is the equation to calculate circumference
     console.log("The circumference of the circle is" +  circumference);
