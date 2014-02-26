1cd4df68a6e9efb207f52323b1185d3e5aafbe10
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-26 13:28:09 -0500

    makes new prompt for stung section

--- week 4/Functions_Worksheet/js/script.js
+++ week 4/Functions_Worksheet/js/script.js
@@ -8,7 +8,7 @@ var r = prompt("What is the radius of your circle?");
 
 function circumference(){
     //circumference is radius times pie
-    var radius = r;
+    var radius = r; //this is the number placed in my the prompt for the radius
     var pie = 3.14; //this is how much pie equals
     var circumference = radius * pie; //this is the equation to calculate circumference
     console.log("The circumference of the circle is" +  circumference);
@@ -18,4 +18,6 @@ circumference();
 
 
 //Stung!
+
+var pounds = prompt("How much do you weigh in pounds? ");
 
