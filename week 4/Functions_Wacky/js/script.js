e199b0462776202eeb3d0d36bef2b2222636d792
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-27 17:46:36 -0500

    fixes anonymous function and changes time number in the return

--- week 4/Functions_Wacky/js/script.js
+++ week 4/Functions_Wacky/js/script.js
@@ -6,8 +6,9 @@
 
 var gummyBears = function(candy, time){
     //how many gummy bears are eaten in an hour
-    var perHour = candy * time;
+    var perHour = candy * time; //the amount of candy times the amount of hours
+  console.log(perHour);
 }
 
-gummyBears(20, 60);
+gummyBears(20, 6);
 
