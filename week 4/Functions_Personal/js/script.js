6f33057e73e1d3eab6aeebd98af71fef73de9830
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-27 16:20:44 -0500

    sets up function with parameters that work

--- week 4/Functions_Personal/js/script.js
+++ week 4/Functions_Personal/js/script.js
@@ -4,3 +4,9 @@
 //Functions Assignment - Personal
 
 
+function averageHeight(year1, year2, year3){
+    var average = (year1 + year2 + year3)/3;
+    console.log(average);
+}
+
+averageHeight(11, 12, 13);
