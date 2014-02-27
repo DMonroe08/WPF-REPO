a698a45d4279e43e80f5be2948c33e6d9cd8f63f
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-27 15:35:52 -0500

    sets up "how tall" variables

--- week 4/Functions_Personal/js/script.js
+++ week 4/Functions_Personal/js/script.js
@@ -3,3 +3,11 @@
  */
 //Functions Assignment - Personal
 
+var year1 = prompt("How tall was your child during the first year?");
+var year2 = prompt("How tall was your child during the second year?");
+var year3 = prompt("How tall was your child during the third year?");
+
+
+function averageHeight(){
+
+}
