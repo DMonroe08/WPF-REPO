40b377415e5f664d0c9b0b1e912d657ce313ebc4
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-27 16:15:11 -0500

    trying to make this work

--- week 4/Functions_Personal/js/script.js
+++ week 4/Functions_Personal/js/script.js
@@ -3,11 +3,4 @@
  */
 //Functions Assignment - Personal
 
-var year1 = prompt("How many inches was your child during the first year?");
-var year2 = prompt("How many inches was your child during the second year?");
-var year3 = prompt("How many inches was your child during the third year?");
 
-
-function averageHeight(){
-
-}
