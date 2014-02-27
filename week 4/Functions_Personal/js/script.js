3c47672c1e43ba8f6d54ab6a0253431382f5df6c
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-27 16:37:28 -0500

    adds comments and changes parameters

--- week 4/Functions_Personal/js/script.js
+++ week 4/Functions_Personal/js/script.js
@@ -3,12 +3,12 @@
  */
 //Functions Assignment - Personal
 
-alert("Functions with parameters were hard");
-alert("But I got it!");
+alert("Functions with parameters were hard"); //first alert message
+alert("But I got it!"); //second alert message
 
 function averageHeight(year1, year2, year3){
-    var average = (year1 + year2 + year3)/3;
+    var average = (year1 + year2 + year3)/3; //equation to make the average
     console.log("My child's average height is" + average + "inches");
 }
 
-averageHeight(11, 12, 13);
+averageHeight(21, 34, 48);
