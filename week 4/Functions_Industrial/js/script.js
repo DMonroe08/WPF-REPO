f641f033960d8d24a430f0154ba66cb758c26384
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-27 15:03:57 -0500

    creates cost function

--- week 4/Functions_Industrial/js/script.js
+++ week 4/Functions_Industrial/js/script.js
@@ -1,3 +1,18 @@
 /**
  * Created by DanielleMonroe on 2/26/14.
  */
+//Functions Industrial
+
+//Planning a trip
+
+var patrons = prompt("How many of you will be joining us in beautiful Paris?");
+
+
+function cost(){
+    var price = 2500;
+    var total = price * patrons;
+    console.log("It will cost" + total + "for your trip.");
+}
+
+cost();
+
