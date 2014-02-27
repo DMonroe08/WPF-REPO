d42759358442a3a3601453486c72ee3b54430c41
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-27 17:37:20 -0500

    creates gummy bear anonymous function

--- week 4/Functions_Wacky/js/script.js
+++ week 4/Functions_Wacky/js/script.js
@@ -1,3 +1,13 @@
 /**
  * Created by DanielleMonroe on 2/27/14.
  */
+
+//Functions Wacky
+
+var gummyBears = function(candy, time){
+    //how many gummy bears are eaten in an hour
+    var perHour = candy * time;
+}
+
+gummyBears(20, 60);
+
