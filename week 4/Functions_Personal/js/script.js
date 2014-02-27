19a5dbb05b7e020d218654252a45cf9a23f6e917
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-27 16:24:05 -0500

    adds two alert messages

--- week 4/Functions_Personal/js/script.js
+++ week 4/Functions_Personal/js/script.js
@@ -3,6 +3,8 @@
  */
 //Functions Assignment - Personal
 
+alert("Functions with parameters were hard");
+alert("But I got it!");
 
 function averageHeight(year1, year2, year3){
     var average = (year1 + year2 + year3)/3;
