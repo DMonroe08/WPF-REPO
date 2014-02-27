c1c3e462b3c2f7170f5a5d731510ab1c963103f1
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-27 17:38:05 -0500

    testing

---
+++ week 4/blah/js/blah.js
@@ -0,0 +1,12 @@
+/**
+ * Created by DanielleMonroe on 2/27/14.
+ */
+
+
+
+function averageHeight(year1, year2, year3){
+    var average = (year1 + year2 + year3)/3;
+    console.log(average);
+}
+
+averageHeight(11, 12, 13);
