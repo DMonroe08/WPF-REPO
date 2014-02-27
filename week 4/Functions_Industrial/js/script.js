a290252d918f9848f5baa83d712adf312f6b16fa
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-27 15:24:28 -0500

    adds comments

--- week 4/Functions_Industrial/js/script.js
+++ week 4/Functions_Industrial/js/script.js
@@ -9,12 +9,13 @@ var patrons = prompt("How many of you will be joining us in beautiful Paris?");
 
 
 function cost(){
-    var price = 2500;
-    var total = price * patrons;
+    var price = 2500; //price per person for the trip
+    var total = price * patrons; //how much the total trip will cost for the amount of patrons
     console.log("It will cost" + total + "for your trip.");
 }
 
 cost();
 
 discount = (patrons < 5) ?  "Add people for a discount." : "You get a 10% discount"
+    //determines if whether or not there will be a discount
 console.log(discount);
